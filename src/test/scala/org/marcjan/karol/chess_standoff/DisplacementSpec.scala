a12b9d22209e85d9a2037136c486514552a0c0da
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-10 20:21:14 +0100

    Add spec for (2, 2) being diagonal

--- src/test/scala/org/marcjan/karol/chess_standoff/DisplacementSpec.scala
+++ src/test/scala/org/marcjan/karol/chess_standoff/DisplacementSpec.scala
@@ -9,4 +9,8 @@ class DisplacementSpec extends FlatSpec with Matchers {
   "(1, 1)" should "be diagonal" in {
     isDiagonal((1, 1)) should be (true)
   }
+
+  "(2, 2)" should "be diagonal" in {
+    isDiagonal((2, 2)) should be (true)
+  }
 }
