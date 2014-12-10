68cb41e2b8c51c162dd1f961f4198607184037b3
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-10 20:18:10 +0100

    Add Displacement{,.isDiagonal

---
+++ src/main/scala/org/marcjan/karol/chess_standoff/Displacement.scala
@@ -0,0 +1,13 @@
+package org.marcjan.karol.chess_standoff
+
+object Displacement {
+
+  /**
+   * Returns true when the given displacement is diagonal and false otherwise.
+   * It is left unspecified for the zero displacement.
+   *
+   * @param displacement a displacement as a tuple of (xDelta, yDelta)
+   * @return is the displacement diagonal?
+   */
+  def isDiagonal(displacement: (Int, Int)): Boolean = false
+}
