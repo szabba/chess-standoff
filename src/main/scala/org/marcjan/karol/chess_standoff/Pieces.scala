4a3006e2c6820e4ffa8e6cb3672cc2a8976c7b7d
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-10 17:24:16 +0100

    Add concrete Piece objects

--- src/main/scala/org/marcjan/karol/chess_standoff/Pieces.scala
+++ src/main/scala/org/marcjan/karol/chess_standoff/Pieces.scala
@@ -4,3 +4,34 @@ package org.marcjan.karol.chess_standoff
  * A chess piece.
  */
 sealed abstract class Piece
+
+/**
+ * A King piece. It can move in any of the eight directions but only by one
+ * square.
+ */
+object King extends Piece
+
+/**
+ * A Queen piece. It can move along a straight line in any of the eight
+ * directions by any number of squares.
+ */
+object Queen extends Piece
+
+/**
+ * A Rook piece. It can move along a straight line in the four non-diagonal
+ * directions by any number of squares.
+ */
+object Rook extends Piece
+
+/**
+ * A Bishop piece. It can move along a straight line in the four diagonal
+ * directions by any number of squares.
+ */
+object Bishop extends Piece
+
+/**
+ * A Knight piece. It moves by two squares in one of the non-diagonal
+ * directions and then by two squares in a direction perpendicular to the first
+ * one.
+ */
+object Knight extends Piece
