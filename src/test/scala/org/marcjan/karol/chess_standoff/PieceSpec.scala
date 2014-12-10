7c8653efdefb892582d8be7d1c8756a3a0c47cc5
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-10 18:04:15 +0100

    Add test for King's movement

--- src/test/scala/org/marcjan/karol/chess_standoff/PieceSpec.scala
+++ src/test/scala/org/marcjan/karol/chess_standoff/PieceSpec.scala
@@ -1,6 +1,9 @@
 package org.marcjan.karol.chess_standoff
 
-import org.scalatest.FlatSpec
+import org.scalatest.{Matchers, FlatSpec}
 
-class PieceSpec extends FlatSpec {
+class PieceSpec extends FlatSpec with Matchers {
+  "A King" should "be able to move by one square in any direction" in {
+    King.canMoveBy((1, 0)) should be (true)
+  }
 }
