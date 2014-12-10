af4f212ea48edee41c7ea2def70392ee1ea33b6f
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-10 18:19:50 +0100

    Add test for King's disallowed movements

--- src/test/scala/org/marcjan/karol/chess_standoff/PieceSpec.scala
+++ src/test/scala/org/marcjan/karol/chess_standoff/PieceSpec.scala
@@ -15,4 +15,26 @@ class PieceSpec extends FlatSpec with Matchers {
     King.canMoveBy((-1, 1)) should be (true)
     King.canMoveBy((1, -1)) should be (true)
   }
+
+  it should "not be able to move beyond one square in any direction" in {
+    King.canMoveBy((2, 2)) should be (false)
+    King.canMoveBy((2, 1)) should be (false)
+    King.canMoveBy((2, 0)) should be (false)
+    King.canMoveBy((2, -1)) should be (false)
+    King.canMoveBy((2, -2)) should be (false)
+
+    King.canMoveBy((1, -2)) should be (false)
+    King.canMoveBy((0, -2)) should be (false)
+    King.canMoveBy((-1, -2)) should be (false)
+    King.canMoveBy((-2, -2)) should be (false)
+
+    King.canMoveBy((-2, -1)) should be (false)
+    King.canMoveBy((-2, 0)) should be (false)
+    King.canMoveBy((-2, 1)) should be (false)
+    King.canMoveBy((-2, 2)) should be (false)
+
+    King.canMoveBy((-1, 2)) should be (false)
+    King.canMoveBy((0, 2)) should be (false)
+    King.canMoveBy((1, 2)) should be (false)
+  }
 }
