daff9d9c7d8e8cde69ef8cbc2b0dbad4c161de1c
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-10 21:02:30 +0100

    Add specs for Displacement.isAlongBoardEdge

--- src/test/scala/org/marcjan/karol/chess_standoff/DisplacementSpec.scala
+++ src/test/scala/org/marcjan/karol/chess_standoff/DisplacementSpec.scala
@@ -25,4 +25,35 @@ class DisplacementSpec extends FlatSpec with Matchers {
   it should "not hold for (-3, 2)" in {
     isDiagonal((-3, 2)) should be (false)
   }
+
+  "isAlongBoardEdge" should "hold for (0, 1)" in {
+    isAlongBoardEdge((0, 1)) should be (true)
+  }
+
+  it should "hold for (0, 2)" in {
+    isAlongBoardEdge((0, 2)) should be (true)
+  }
+
+  it should "hold for (1, 0)" in {
+    isAlongBoardEdge((1, 0)) should be (true)
+  }
+
+  it should "hold for (0, -1)" in {
+    isAlongBoardEdge((0, -1)) should be (true)
+  }
+  it should "hold for (-1, 0)" in {
+    isAlongBoardEdge((-1, 0)) should be (true)
+  }
+
+  it should "not hold for (1, 2)" in {
+    isAlongBoardEdge((1, 2)) should be (false)
+  }
+
+  it should "not hold for (1, 1)" in {
+    isAlongBoardEdge((1, 1)) should be (false)
+  }
+
+  it should "not hold for (-1, 1)" in {
+    isAlongBoardEdge((-1, 1)) should be (false)
+  }
 }
