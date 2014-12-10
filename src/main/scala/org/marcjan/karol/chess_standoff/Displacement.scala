db250241900edbbe7cc7cef1ca33b835a68f2ce4
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-10 20:55:16 +0100

    Add Displacement.isAlongBoardEdge

--- src/main/scala/org/marcjan/karol/chess_standoff/Displacement.scala
+++ src/main/scala/org/marcjan/karol/chess_standoff/Displacement.scala
@@ -13,4 +13,13 @@ object Displacement {
     displacement match {
       case (xDelta, yDelta) => xDelta == yDelta
     }
+
+  /**
+   * Returns true when the given displacement is along the board's edge. It is
+   * left unspecified for the zero displacement.
+   *
+   * @param displacement a displacement as a tuple of (xDelta, yDelta)
+   * @return is the displacement along the board's edge?
+   */
+  def isAlongBoardEdge(displacement: (Int, Int)): Boolean = false
 }
