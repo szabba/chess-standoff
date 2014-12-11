c9deb042eeca7d50a3811781048397d08b80ac0c
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-11 14:27:54 +0100

    Add column value to Position class

--- src/main/scala/org/marcjan/karol/chess_standoff/Position.scala
+++ src/main/scala/org/marcjan/karol/chess_standoff/Position.scala
@@ -3,4 +3,6 @@ package org.marcjan.karol.chess_standoff
 /**
  * A position of a piece on the chessboard.
  */
-class Position(val row: Int, someColumn: Int)
+class Position(val row: Int, someColumn: Int) {
+  val column = 0
+}
