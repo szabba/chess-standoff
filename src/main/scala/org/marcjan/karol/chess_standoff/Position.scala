f36f13228583d48fbc0d2bd03112eda1ebc2c970
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-11 14:59:09 +0100

    Add == to Position class

--- src/main/scala/org/marcjan/karol/chess_standoff/Position.scala
+++ src/main/scala/org/marcjan/karol/chess_standoff/Position.scala
@@ -6,7 +6,16 @@ package org.marcjan.karol.chess_standoff
  * @param row row containing the square
  * @param column column containing the square
  */
-class Position(val row: Int, val column: Int)
+class Position(val row: Int, val column: Int) {
+
+  /**
+   * Returns true if the value is equal to p.
+   *
+   * @param p position to compare to
+   * @return are the positions equal?
+   */
+  def ==(p: Position): Boolean = false
+}
 
 /**
  * Associate object of Position class.
