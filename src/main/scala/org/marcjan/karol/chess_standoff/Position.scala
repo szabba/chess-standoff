ba5ddb3606c14c4d021c638a9ee0c9d08b6f113e
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-11 14:41:26 +0100

    Document Position class parameters

--- src/main/scala/org/marcjan/karol/chess_standoff/Position.scala
+++ src/main/scala/org/marcjan/karol/chess_standoff/Position.scala
@@ -1,6 +1,9 @@
 package org.marcjan.karol.chess_standoff
 
 /**
- * A position of a piece on the chessboard.
+ * A position of a square on the chessboard.
+ *
+ * @param row row containing the square
+ * @param column column containing the square
  */
 class Position(val row: Int, val column: Int)
