fdfedb63e78e16a6e2943a773dfc02c540722b84
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-11 21:59:11 +0100

    Add spec for columns val on Board class

--- src/test/scala/BoardSpec.scala
+++ src/test/scala/BoardSpec.scala
@@ -7,10 +7,19 @@ class BoardSpec extends UnitSpec {
 
   "A Board" should "have given number of rows" in {
     val rows = 7
-    val cols = 6
+    val columns = 6
 
-    val board = new Board(rows, cols)
+    val board = new Board(rows, columns)
 
     board.rows should be (rows)
+  }
+
+  it should "have given number of columns" in {
+    val rows = 7
+    val columns = 6
+
+    val board = new Board(rows, columns)
+
+    board.columns should be (columns)
   }
 }
