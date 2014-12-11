7dcd75d080f122853d3821dedaae9bd442b053f2
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-11 14:31:57 +0100

    Pass last added spec

--- src/main/scala/org/marcjan/karol/chess_standoff/Position.scala
+++ src/main/scala/org/marcjan/karol/chess_standoff/Position.scala
@@ -3,6 +3,4 @@ package org.marcjan.karol.chess_standoff
 /**
  * A position of a piece on the chessboard.
  */
-class Position(val row: Int, someColumn: Int) {
-  val column = 0
-}
+class Position(val row: Int, val column: Int)
