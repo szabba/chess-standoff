4f1907a8bba0d1d725cc2f205987defed4172fe6
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-11 14:42:47 +0100

    Add apply method to Position object

--- src/main/scala/org/marcjan/karol/chess_standoff/Position.scala
+++ src/main/scala/org/marcjan/karol/chess_standoff/Position.scala
@@ -7,3 +7,20 @@ package org.marcjan.karol.chess_standoff
  * @param column column containing the square
  */
 class Position(val row: Int, val column: Int)
+
+/**
+ * Associate object of Position class.
+ */
+object Position {
+
+  /**
+   * Shortcut for creating Positions without the new keyword.
+   *
+   * @param row row containing the square
+   * @param column column containing the square
+   * @return a position
+   */
+  def apply(row: Int, column: Int): Position = {
+    new Position(0, 0)
+  }
+}
