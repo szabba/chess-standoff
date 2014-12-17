db2f0ef1e3990fcd7b7527836ab4b2af22f45bdb
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-18 00:20:53 +0100

    Document Main object

--- src/main/scala/org/marcjan/karol/chess_standoff/Main.scala
+++ src/main/scala/org/marcjan/karol/chess_standoff/Main.scala
@@ -2,6 +2,12 @@ package org.marcjan.karol.chess_standoff
 
 import java.util.Scanner
 
+/**
+ * Reads the board dimensions (first rows, then columns) and the number of
+ * pieces of each kind (in order: (K)ings, (Q)ueens, (R)ooks, (B)ishops and
+ * K(N)ights) to place on the board. Then prints out all the valid placement of
+ * the given piece set on a board of the given size and states their number.
+ */
 object Main extends scala.App {
 
   val pieceKindOrder = List(King, Queen, Rook, Bishop, Knight)
