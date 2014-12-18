23bcf6530c8e5b97c74a0e81e039d4fffbf5e113
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-18 14:10:02 +0100

    Add Symmetry trait

---
+++ src/main/scala/org/marcjan/karol/chess_standoff/Symmetry.scala
@@ -0,0 +1,36 @@
+package org.marcjan.karol.chess_standoff
+
+/**
+ * A Symmetry can transform between "morally equivalent" boards.
+ */
+private sealed trait Symmetry {
+
+  /**
+   * Returns the board transformed by the symmetry.
+   *
+   * @param board board to transform
+   * @return board transformed by the symmetry
+   */
+  def apply(board: Board): Board = {
+
+    val pieces = board.pieces map {
+      case piece => {
+        piece.kind(
+          apply(board.rows, board.columns, piece.position))
+      }
+    }
+
+    new Board(board.rows, board.columns, pieces)
+  }
+
+  /**
+   * Returns a position transformed through the symmetry assuming the given
+   * board dimensions.
+   *
+   * @param rows number of board rows
+   * @param columns number of board columns
+   * @param position position to transform
+   * @return transformed position
+   */
+  def apply(rows: Int, columns: Int, position: Position): Position
+}
