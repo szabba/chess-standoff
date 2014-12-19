c38bd09370c5eb1887e33de60a568a8338fbb545
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-19 23:37:07 +0100

    Add SymmetrySearch.symmetryGroup

--- src/main/scala/org/marcjan/karol/chess_standoff/SymmetrySearch.scala
+++ src/main/scala/org/marcjan/karol/chess_standoff/SymmetrySearch.scala
@@ -4,4 +4,26 @@ private object SymmetrySearch {
 
   type Nexter = Position => Position
   type KindCounts = Map[PieceKind, Int]
+
+  private val rectangleSymmetries = Set(
+    Identity, MirrorRows, MirrorColumns, RotateRightBy180Degrees)
+
+  private val squareOnlySymmetries = Set(
+    MirrorDiagonal, MirrorAntidiagonal,
+    RotateRightBy90Degrees, RotateRightBy270Degrees)
+
+  /**
+   * Returns the list of all symmetries of boards with the given dimensions.
+   *
+   * @param rows number of board rows
+   * @param columns number of board columns
+   * @return list of symmetries that apply to boards with the given numbers of
+   *         rows and dimmensions
+   */
+  def symmetryGroup(rows: Int, columns: Int): Set[Symmetry] = {
+    if (rows != columns)
+      rectangleSymmetries
+    else
+      rectangleSymmetries ++ squareOnlySymmetries
+  }
 }
