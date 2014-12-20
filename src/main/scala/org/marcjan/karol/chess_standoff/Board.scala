75b4a6b05598bc3fca87c42f6b227f488cd49cc1
Karol Marcjan <karol.marcjan@gmail.com>
2014-12-20 19:31:29 +0100

    Drop lazy qualifier from Board.safeSpace

--- src/main/scala/org/marcjan/karol/chess_standoff/Board.scala
+++ src/main/scala/org/marcjan/karol/chess_standoff/Board.scala
@@ -40,7 +40,7 @@ class Board(val rows: Int, val columns: Int, val pieces: Seq[Piece] = Nil, space
 
   override def hashCode: Int = 41 + toString.hashCode
 
-  private[chess_standoff] lazy val safeSpace = {
+  private[chess_standoff] val safeSpace = {
     space match {
       case None => {
         for {
